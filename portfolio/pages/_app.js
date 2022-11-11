dd17537d1d657f392c82291af16ab221b971877d
arjitjohar <arjit39@my.yorku.ca>
2022-11-11 12:43:28 -0500

    added pages

--- portfolio/pages/_app.js
+++ portfolio/pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "./components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <Layout>
+    return <Component {...pageProps} />;
+  </Layout>;
 }
 
-export default MyApp
+export default MyApp;
